283d184b56b599baf439a6101dc2b92adf07d717
fedeghe <fedeghe@gmail.com>
2020-02-22 16:59:08 +0100

    lintz

--- integrate.js
+++ integrate.js
@@ -2,7 +2,7 @@ module.exports = (fn, a, b, dx = 0.01) => {
     const precisionMatch = `${dx}`.match(/\.(\d+)/),
         precision = precisionMatch ? precisionMatch[1].length : 0,
         steps = (b - a) / dx,
-        arr = Array.from({length: steps}, (x, i)=>i),
+        arr = Array.from({length: steps}, (x, i) => i),
         res =  arr.reduce(
             (acc, el, i) => acc + fn(a + i * dx+ dx/2) * dx,
         0)
