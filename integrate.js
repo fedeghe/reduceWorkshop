578ac0d2fb1028c59b0181e73b69bafe40979a6b
fedeghe <fedeghe@gmail.com>
2020-02-23 14:48:33 +0100

    a meaningful comment

--- integrate.js
+++ integrate.js
@@ -1,3 +1,8 @@
+/**
+ * This is an experimental function to calculate the value of a
+ * defined integral of a function ∈ C¹ (in a defined interval, obviously)
+ * ----------------
+ */
 module.exports = (fn, a, b, dx = 0.01) => {
     const precisionMatch = `${dx}`.match(/\.(\d+)/),
         precision = precisionMatch ? precisionMatch[1].length : 0,
