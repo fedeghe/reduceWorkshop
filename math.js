c0bb69e3a2eba06c13d571ae2cc11ddb0c2bf802
fedeghe <fedeghe@gmail.com>
2020-02-25 21:41:38 +0100

    zzz

--- math.js
+++ math.js
@@ -16,7 +16,7 @@ const integrate = (fn, a, b, dx = 0.01) => {
         steps = (b - a) / dx,
         arr = Array.from({length: steps}, () => 0),
         res =  arr.reduce(
-            (acc, el, i) => acc + fn(a + i * dx+ dx / 2) * dx,
+            (acc, el, i) => acc + fn(a + i * dx + dx / 2) * dx,
         0);
     return withPrecisionDx(res);
 };
@@ -26,6 +26,7 @@ const derive = (fn, a, dx = 0.01) => {
         mid = dx / 2;
     return withPrecisionDx((fn(a + mid) - fn(a - mid)) / dx);
 }
+
 module.exports = {
     integrate,
     derive
