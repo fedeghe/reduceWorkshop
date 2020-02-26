020cdef74b659918748f1431697ecef2d7f6cef9
fedeghe <fedeghe@gmail.com>
2020-02-26 20:42:25 +0100

    faster?

--- max.js
+++ max.js
@@ -1,4 +1,4 @@
-const fn = (acc, el) => Math.max(acc, el),
+const fn = (acc, el) => acc >  el ? acc : el,
     identityElement = -Infinity;
 
 module.exports = list => list.reduce(fn, identityElement);
--- min.js
+++ min.js
@@ -1,4 +1,4 @@
-const fn = (acc, el) => Math.min(acc, el),
+const fn = (acc, el) => acc > el ? el : acc,
     identityElement = Infinity;
 
 module.exports = list => list.reduce(fn, identityElement);
